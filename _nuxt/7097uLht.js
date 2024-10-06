737575480488e960d42f8aef9ff02d2fb21afb9e
LinX <linxlabs@mail.ru>
2024-10-06 14:43:37 +0300

    Updates

---
+++ _nuxt/7097uLht.js
@@ -0,0 +1,1 @@
+import{bq as l}from"./DY338Vvd.js";const t=l("store",{state:()=>({btBtnText:"Connect",btIcon:"mdi-bluetooth",btStat:!1,btAvailable:!1,apName:"",config:{ssidPrim:null,pwPrim:null,ssidSec:null,pwSec:null,stAddr:null,stUni:null,mode:null,pixels:null,channels:null,fw:null},drawer:!1,items:[{title:"Home",value:"/"},{title:"Configuration",value:"/config"}]})});export{t as u};
