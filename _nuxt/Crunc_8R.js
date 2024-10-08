f2dbabccaec21ba6710bb94331cd8491231465c7
LinX <linxlabs@mail.ru>
2024-10-08 12:07:40 +0300

    Updates

--- _nuxt/Crunc_8R.js
+++
@@ -1,1 +0,0 @@
-import{b as s,V as t}from"./DxigiMty.js";import{ar as e,_ as o,ac as r,A as a,t as c,z as n}from"./DjrJLeFH.js";import{V as p}from"./HWqB0x95.js";const _=e("/logo.png"),i={setup(){return{store:s()}},mounted(){}};function m(u,f,d,l,x,V){return c(),r(t,{class:"mb-5"},{default:a(()=>[n(p,{src:_})]),_:1})}const h=o(i,[["render",m]]);export{h as default};
