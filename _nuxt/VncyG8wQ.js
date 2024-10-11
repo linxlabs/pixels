aa884674c377a672ad793c31fbb293343207b780
LinX <linxlabs@mail.ru>
2024-10-11 10:28:59 +0300

    Updates

--- _nuxt/VncyG8wQ.js
+++
@@ -1,1 +0,0 @@
-import{b as s,V as t}from"./DU2ciyj3.js";import{ar as e,_ as o,ac as r,A as a,t as c,z as n}from"./C6gb5Gth.js";import{V as p}from"./D_8jBAqV.js";const _=e("/logo.png"),i={setup(){return{store:s()}},mounted(){}};function m(u,f,d,l,x,V){return c(),r(t,{class:"mb-5"},{default:a(()=>[n(p,{src:_})]),_:1})}const h=o(i,[["render",m]]);export{h as default};
