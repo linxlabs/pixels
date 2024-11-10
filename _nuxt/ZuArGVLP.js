e4841c18c443d089b2da462845f53b7b75b7eb91
LinX <linxlabs@mail.ru>
2024-11-10 10:57:35 +0300

    Updates

---
+++ _nuxt/ZuArGVLP.js
@@ -0,0 +1,1 @@
+import{b as s,V as t}from"./dogLuteG.js";import{al as e,_ as o,ad as r,A as a,t as c,z as n}from"./CVUqWoOa.js";import{V as p}from"./B_K3rln2.js";const _=e("/logo.png"),i={setup(){return{store:s()}},mounted(){}};function m(u,d,f,l,x,V){return c(),r(t,{class:"mb-5"},{default:a(()=>[n(p,{src:_})]),_:1})}const h=o(i,[["render",m]]);export{h as default};
